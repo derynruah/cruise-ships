5564df35ec5dbc38ff39aa6f8f5c954d67d7d8f4
Deryn Rushworth <derynrushworth@gmail.com>
2022-08-29 16:32:47 +0100

    Added ships to ports

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -9,4 +9,25 @@ describe('Port', () =>{
         const port = new Port ('Calais');
         expect(port.name).toBeTruthy();
     });
+
+    it('can add a ship', () => {
+        const port = new Port('Dover');
+        const ship = {};
+
+        port.addShip(ship);
+
+        expect(port.ships).toContain(ship);
+    });
+
+    it('can remove a ship', () => {
+        const port = new Port('Dover');
+        const titanic = {};
+        const queenMary = {};
+
+        port.addShip(titanic);
+        port.addShip(queenMary);
+        port.removeShip(queenMary);
+
+        expect(port.ships).toEqual([titanic]);
+    });
 });
