b663a52994856d65c1f29a95695d1dc0f20358b5
Deryn Rushworth <derynrushworth@gmail.com>
2022-09-04 17:15:38 +0100

    completed ports have ships

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -21,13 +21,15 @@ describe('Ship', () => {
     });
 
     it('can set sail', () => {
-        const port = new Port('Dover');
-        const itinerary = new Itinerary([port]);
+        const dover = new Port('Dover');
+        const calais = new Port('Calais');
+        const itinerary = new Itinerary([dover, calais]);
         const ship = new Ship(itinerary);
 
         ship.setSail();
 
         expect(ship.currentPort).toBeFalsy();
+        expect(dover.ships).not.toContain(ship);
     });
 
     it('can dock at a different port', () => {
@@ -40,5 +42,26 @@ describe('Ship', () => {
         ship.dock();
 
         expect(ship.currentPort).toBe(calais);
+        expect(calais.ships).toContain(ship);
+    });
+
+    it("can't sail further than its itinerary", () => {
+        const dover = new Port('Dover');
+        const calais = new Port('Calais');
+        const itinerary = new Itinerary([dover, calais]);
+        const ship = new Ship(itinerary);
+
+        ship.setSail();
+        ship.dock();
+
+        expect(() => ship.setSail()).toThrowError('End of itinerary reached');
+    });
+
+    it('gets added to port on instantation', () => {
+        const dover = new Port('Dover');
+        const itinerary = new Itinerary([dover]);
+        const ship = new Ship(itinerary);
+
+        expect(dover.ships).toContain(ship);
     });
 });
